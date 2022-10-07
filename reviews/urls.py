04d7e8e94829ef32d6e239e165f55eaa8d4ddc33
nevertheless0404 <nevertheless.hi@gmail.com>
2022-10-07 14:39:55 +0900

    검색기능 완료하자

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
-app_name ='reviews'
-urlpatterns =[
-    path('', views.index, name='index'),
-    path('create/',views.create, name='create'),
-    path('new/',views.create, name='new'),
-    path('<int:pk>/',views.detail, name='detail'),
-    path('<int:pk>/update/',views.update, name='update'),
-    path('<int:pk>/delete/',views.delete, name='delete'),
+app_name = "reviews"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("new/", views.create, name="new"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/update/", views.update, name="update"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
+    path("search/", views.search, name="search"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,54 +1,83 @@
+from turtle import title
 from django.shortcuts import render, redirect
 from .models import Review
 from .forms import ReviewForm
+from django.db.models import Q
+
 # Create your views here.
 def index(request):
-    reviews = Review.objects.all().order_by('-pk')
+    reviews = Review.objects.all().order_by("-pk")
     cnt = 1
     reviews_list = []
     for i in reviews:
-        reviews_list.append({'num': cnt, 'title': i.title, 'pk':i.id, 'created_at': i.created_at, 'updated_at':i.updated_at})
+        reviews_list.append(
+            {
+                "num": cnt,
+                "title": i.title,
+                "pk": i.id,
+                "created_at": i.created_at,
+                "updated_at": i.updated_at,
+            }
+        )
         cnt += 1
     context = {
-        'reviews': reviews_list,
+        "reviews": reviews_list,
     }
-    return render(request, 'reviews/index.html', context)
+    return render(request, "reviews/index.html", context)
+
 
 def create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
             review_form.save()
-            return redirect('reviews:index')
-    else: 
+            return redirect("reviews:index")
+    else:
         review_form = ReviewForm()
     context = {
-        'review_form' : review_form,
-    }        
-    return render(request, 'reviews/new.html', context)
-
-def detail(request,pk):
-    review = Review.objects.get(pk=pk)
-    context={
-        'review': review,
+        "review_form": review_form,
     }
-    return render(request,'reviews/detail.html',context)
+    return render(request, "reviews/new.html", context)
 
-def update (request,pk):
+
+def detail(request, pk):
     review = Review.objects.get(pk=pk)
-    if request.method == 'POST':
-        review_form = ReviewForm(request.POST,instance=review)
+    context = {
+        "review": review,
+    }
+    return render(request, "reviews/detail.html", context)
+
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, instance=review)
         if review_form.is_valid():
             review_form.save()
-            return redirect('reviews:detail',review.pk)
-    else: 
+            return redirect("reviews:detail", review.pk)
+    else:
         review_form = ReviewForm(instance=review)
     context = {
-        'review_form' : review_form,
-    }        
-    return render(request, 'reviews/update.html', context)
+        "review_form": review_form,
+    }
+    return render(request, "reviews/update.html", context)
 
-def delete (request,pk):
+
+def delete(request, pk):
     review = Review.objects.get(pk=pk)
     review.delete()
-    return redirect('reviews:index')    
+    return redirect("reviews:index")
+
+
+def search(request):
+    if request.method == "GET":
+        content_list = Review.objects.all()
+        search = request.GET.get("search", "")
+
+        if search:
+            search_list = content_list.filter(
+                Q(title__icontains=search)
+                | Q(content__icontains=search)
+                | Q(movie_name__icontains=search)
+            )
+            return render(request, "reviews/search.html", {"search": search_list})
